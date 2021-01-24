3568be27b8a5c065c212c312951851072008ae95
Archit Pandey <archit.pandey@deskera.org>
2021-01-24 22:31:51 +0530

    add google search verification
    Signed-off-by: Archit Pandey <archit.pandey@deskera.org>

--- src/components/PageMeta.js
+++ src/components/PageMeta.js
@@ -9,6 +9,7 @@ function PageMeta({ meta }) {
       <title>{meta.title}</title>
       <meta name="description" content={meta.description} />
       <meta name="keywords" content={meta.keywords} />
+      <meta name="google-site-verification" content="uyH_CpYLfeNf0knUsy21H9I4khlWMY68MSl41Ah65Cc" />
       <link rel="icon" href={Logo} type="image/png" />
     </Helmet>
   )
