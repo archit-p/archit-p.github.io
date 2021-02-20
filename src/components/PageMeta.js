e0890722530636a01e536e0ef208ef2781438b83
Archit Pandey <archit.pandey@deskera.org>
2021-02-20 22:37:38 +0530

    remove favicon
    Signed-off-by: Archit Pandey <archit.pandey@deskera.org>

--- src/components/PageMeta.js
+++ src/components/PageMeta.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Helmet } from "react-helmet"
-import Logo from "src/assets/logo.png"
 
 function PageMeta({ meta }) {
   return (
@@ -10,7 +9,6 @@ function PageMeta({ meta }) {
       <meta name="description" content={meta.description} />
       <meta name="keywords" content={meta.keywords} />
       <meta name="google-site-verification" content="uyH_CpYLfeNf0knUsy21H9I4khlWMY68MSl41Ah65Cc" />
-      <link rel="icon" href={Logo} type="image/png" />
     </Helmet>
   )
 }
